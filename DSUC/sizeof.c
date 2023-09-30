59da5a4eab46570356c526c6aa5df4ec1dc011c5
Santanu Pradhan <santanuworkspace@gmail.com>
2023-10-01 02:18:41 +0530

    Write a program in C to find the size of int, float, double and char

---
+++ DSUC/sizeof.c
@@ -0,0 +1,8 @@
+// Write a program in C to find the size of int, float, double and char.
+#include <stdio.h>
+int main(){
+	printf("Size of 'int' type is %d", sizeof(int));
+	printf("\nSize of 'float' type is %d", sizeof(float));
+	printf("\nSize of 'double' type is %d", sizeof(double));
+	printf("\nSize of 'char' type is %d", sizeof(char));
+}
